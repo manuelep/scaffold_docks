558d7f3e0f9944360d6d7e9a6f0c4ac0751bd2db
Manuele Pesenti <manuele.pesenti@gter.it>
2023-07-25 15:51:57 +0200

    PYDAL_URL variable in schaffold application settings

--- apps/_scaffold/settings.py
+++ apps/_scaffold/settings.py
@@ -14,7 +14,8 @@ APP_NAME = os.path.split(APP_FOLDER)[-1]
 # DB_FOLDER:    Sets the place where migration files will be created
 #               and is the store location for SQLite databases
 DB_FOLDER = required_folder(APP_FOLDER, "databases")
-DB_URI = "sqlite://storage.db"
+# DB_URI = "sqlite://storage.db"
+DB_URI = os.getenv('PYDAL_URI')
 DB_POOL_SIZE = 1
 DB_MIGRATE = True
 DB_FAKE_MIGRATE = False  # maybe?
